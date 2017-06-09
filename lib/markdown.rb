a33ea525a008c6ac43a9236e6764179a6a7b02c9
celilileri <celil.ileri@bil.omu.edu.tr>
2017-06-10 00:29:42 +0300

    Sadece bu modülün yapacaklarını ekle

--- lib/markdown.rb
+++ lib/markdown.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 
-# TODO: Markdown dosya adını arguman olarak al
-#       Aç ve parse et
-#       Başlıklara göre parçala
-#       Bir veritabanı yada dosyada parçalanmış halini tut
-#       İstenen parçayı döndür
-#       Gönderilen parçayı al
-#       İlgili parçayla değiştir / birleştir
-
+module Markdown
+  # TODO: Markdown dosya adını arguman olarak al
+  #       Aç ve parse et
+  #       Başlıklara göre parçala
+  #       Bir veritabanı yada dosyada parçalanmış halini tut
+  #       İstenen parçayı döndür
+  #       Gönderilen parçayı al
+  #       İlgili parçayla değiştir / birleştir
+end
