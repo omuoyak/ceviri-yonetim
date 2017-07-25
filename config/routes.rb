7b8b0f2226787175a86ca9d8cf5783fd4d2151a8
wemrekurt <emre.kurt@bil.omu.edu.tr>
2017-07-25 11:59:41 +0300

    Sadece github ile giriş

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"}
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"}, :skip => [:registrations]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
