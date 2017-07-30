fec870e7a294cfa1b6bdddd97bccb53f922aa073
celilileri <celil.ileri@bil.omu.edu.tr>
2017-07-30 23:49:04 +0300

    Translation için rotaları ekle

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :translations
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"}, :skip => [:registrations]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
